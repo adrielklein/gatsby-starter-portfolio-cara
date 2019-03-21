2ac84b6886a2013325d71ee5f612b96608bafda4
Adriel <adrielmklein@gmail.com>
2019-03-20 23:56:27 -0700

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -63,9 +63,9 @@ const Index = () => (
     <Parallax pages={5}>
       <Hero offset={0}>
         <BigTitle>
-          Hello, <br /> I'm John Doe.
+          Hello, <br /> I'm Adriel Klein.
         </BigTitle>
-        <Subtitle>I'm creating noice web experiences for the next generation of consumer-facing companies.</Subtitle>
+        <Subtitle>I'm an engineer eager to add value to the world by bringing clever software visions to life.</Subtitle>
       </Hero>
       <Projects offset={1}>
         <Title>Projects</Title>
